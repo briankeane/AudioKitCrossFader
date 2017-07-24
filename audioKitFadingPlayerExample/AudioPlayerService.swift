c325750d188933237eea2a257acb581233895a4a
briankeane <lonesomewhistle@gmail.com>
2017-07-24 08:56:05 -0500

    barebones audioplayer created

---
+++ audioKitFadingPlayerExample/AudioPlayerService.swift
@@ -0,0 +1,13 @@
+//
+//  AudioPlayerService.swift
+//  audioKitFadingPlayerExample
+//
+//  Created by Brian D Keane on 7/24/17.
+//  Copyright © 2017 Brian D Keane. All rights reserved.
+//
+
+import UIKit
+
+class AudioPlayerService: NSObject {
+
+}
---
+++ audioKitFadingPlayerExample/SpinModel.swift
@@ -0,0 +1,9 @@
+//
+//  SpinModel.swift
+//  audioKitFadingPlayerExample
+//
+//  Created by Brian D Keane on 7/24/17.
+//  Copyright © 2017 Brian D Keane. All rights reserved.
+//
+
+import Foundation
